cc4d1e7bf7b217b39ea695ee4bee39d100a5fc21
Simone <simone.barbaro2@studio.unibo.it>
2020-03-23 09:36:52 +0100

    added boolean filters

--- src/filter.py
+++ src/filter.py
@@ -1,20 +1,59 @@
 import numpy as np
 from keyword_selection import InvertedIndex, KeywordSelector
+from functools import reduce
+
+
+class FilterStrategy:
+    """
+    Interface of the corpus filter based on the results of the keyword selection.
+    """
+    def filter_selection(self, selected_doc_ids):
+        """
+        Strategy used to decide how to filter documents based on which contains what keys.
+        There are many possible solutions and the best must be found with experimentation.
+        :param selected_doc_ids: a list of lists of documents ids associated with different keywords.
+        :return: a list of document ids selected according to the specific strategy.
+        """
+        raise NotImplementedError
+
+
+class AndFilterStrategy(FilterStrategy):
+    """
+    Implementation of filter strategy that take the intersection of the documents.
+    Said in an other way, it implements the and operation on the keywords boolean filter.
+    """
+
+    def filter_selection(self, selected_doc_ids):
+        return reduce(np.intersect1d, selected_doc_ids)
+
+
+class OrFilterStrategy(FilterStrategy):
+    """
+    Implementation of filter strategy that take the union of the documents.
+    Said in an other way, it implements the or operation on the keywords boolean filter.
+    """
+
+    def filter_selection(self, selected_doc_ids):
+        return reduce(np.union1d, selected_doc_ids)
 
 
 class CorpusFilter:
     """
     Base class to filter corpus based on keyword methods.
     """
-    def __init__(self, corpus, preprocessing_fn, keyword_selector: KeywordSelector):
+    def __init__(self, corpus, preprocessing_fn, keyword_selector: KeywordSelector, filter_strategy: FilterStrategy):
         """
         :param corpus: corpus to be filtered.
         :param preprocessing_fn: function to preprocess and tokenize corpus.
         :param keyword_selector: object that select keywords based on query.
+        :param filter_strategy: strategy used to decide how to filter documents based on which contains what keys.
+        There are many possible solutions and the best must be found with experimentation.
+        It takes a list of document ids and return a filtered list.
         """
         self.inverted_index = InvertedIndex(corpus, preprocessing_fn)
         self.preprocessing_fn = preprocessing_fn
         self.keyword_selector = keyword_selector
+        self.filter_strategy = filter_strategy
 
     def selection_strategy(self, tokens):
         """
@@ -22,16 +61,6 @@ class CorpusFilter:
         There are many possible solutions and the best must be found with experimentation.
         :param tokens: tokens from which to select the keywords.
         :return: a list of keywords
-        """
-        raise NotImplementedError
-
-    def filter_strategy(self, selected_doc_ids):
-        """
-        Abstract method used to decide how to filter documents based on which contains what keys.
-        There are many possible solutions and the best must be found with experimentation.
-        :param selected_doc_ids: array where each element contains an array containing all the ids of the documents
-        containing a specific keyword.
-        :return: a list of doc_ids filtered by this method.
         """
         raise NotImplementedError
 
@@ -44,4 +73,17 @@ class CorpusFilter:
         tokens = self.preprocessing_fn(query)
         selected_keywords = self.selection_strategy(tokens)
         selected_doc_ids = self.inverted_index.get_doc_id_list(selected_keywords)
-        return self.filter_strategy(selected_doc_ids)
+        return self.filter_strategy.filter_selection(selected_doc_ids)
+
+
+class SelectKCorpusFilter(CorpusFilter):
+    """
+    Simple corpus filter that select at most k keywords based on the keyword scorer where k is given as input.
+    """
+    def __init__(self, corpus, preprocessing_fn, keyword_selector: KeywordSelector, filter_strategy: FilterStrategy, k):
+        super().__init__(corpus, preprocessing_fn, keyword_selector, filter_strategy)
+        self.k = k
+
+    def selection_strategy(self, tokens):
+        return self.keyword_selector.select_k(tokens, self.k)
+
